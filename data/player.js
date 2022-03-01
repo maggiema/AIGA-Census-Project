38008432969e6e71b4dc761e7c4714368340b33e
doromayli <78432897+doromayli@users.noreply.github.com>
2022-03-01 16:06:23 -0500

    Add files via upload

---
+++ data/player.js
@@ -0,0 +1,22 @@
+const player_data = [
+    {
+      about_card: "highlight_obstacle.png",
+    },
+    {
+      about_card: "highlight_reward.png",
+    },
+    {
+      about_card: "highlight_risk.png",
+    },
+    {
+      about_card: "graphic_card.png",
+    },
+    {
+      about_card: "quote_card.png",
+    },
+    {
+      about_card: "highlight_obstacle.png",
+    },
+];
+
+export default player_data;
